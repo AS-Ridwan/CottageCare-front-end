d472ce3e952cb3c8d3bae85efdda0b7311fb3d8f
hipopridwan <ridwanhipop@gmail.com>
2024-02-15 23:03:18 +0600

    employment page form all input added

--- src/pages/EmploymentOpportunities/EmploymentOpportunities.jsx
+++ src/pages/EmploymentOpportunities/EmploymentOpportunities.jsx
@@ -7,35 +7,73 @@ const EmploymentOpportunities = () => {
     <>
       <section className="w-screen 2xl:px-64 mt-6 ">
         <div>EmploymentOpportunities page</div>
-        <div className=" grid grid-cols-1 sm:grid-cols-2 items-center mt-24">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 items-center  gap-20">
           {/* left side img */}
           {/* left side img */}
-          <div className="h-auto max-w-lg w-2/3  lg:w-screen ">
-            <img src={employment} alt="" />
+          <div className=" flex justify-center  ">
+            <img
+              className="h-auto max-w-lg w-2/3 lg:w-screen"
+              src={employment}
+              alt=""
+            />
           </div>
 
           {/* right side form */}
           {/* right side form */}
-          <div className="flex flex-col items-end justify-center">
+          <div className="flex flex-col items-center  justify-center">
             <div className="w-full bg-accent rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-medium leading-tight tracking-tight text-secondary  md:text-2xl ">
                   Create your account to get full access!
                 </h1>
                 <form className="space-y-4 md:space-y-6" action="#">
+                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    <div>
+                      <label
+                        for="text"
+                        className="block mb-2 text-sm font-medium text-secondary dark:text-accent"
+                      >
+                        First Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                        placeholder="First Name"
+                        required=""
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="text"
+                        className="block mb-2 text-sm font-medium text-secondary dark:text-accent"
+                      >
+                        Last Name
+                      </label>
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                        placeholder="Last Name"
+                        required=""
+                      />
+                    </div>
+                  </div>
                   <div>
                     <label
-                      for="text"
+                      for="number"
                       className="block mb-2 text-sm font-medium text-secondary dark:text-accent"
                     >
-                      Full Name
+                      Phone Number
                     </label>
                     <input
-                      type="text"
-                      name="name"
-                      id="name"
+                      type="number"
+                      name="number"
+                      id="number"
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                      placeholder="name"
+                      placeholder="Your Number"
                       required=""
                     />
                   </div>
@@ -56,77 +94,65 @@ const EmploymentOpportunities = () => {
                     />
                   </div>
                   <div>
-                    <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-secondary "
-                    >
-                      Password
-                    </label>
+                    <label for="choice"> At Least 18 years of Age?</label>
                     <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                      required=""
+                      type="checkbox"
+                      id="choice"
+                      className="ms-3"
+                      name="choice"
+                      value=""
                     />
+                  </div>
+
+                  <div>
+                    <label for="input">
+                      {" "}
+                      Interested in Which Metro Area?(Required)
+                    </label>
+
+                    <select className=" select select-bordered  w-full ">
+                      <option selected>Akron, OH</option>
+                      <option>Barboursville, WV</option>
+                      <option>Baton Rouge, LA</option>
+                      <option>Bloomington, IL</option>
+                      <option>Calgary North Central</option>
+                      <option>Calgary South</option>
+                      <option>Milwaukee/North, WI</option>
+                    </select>
                   </div>
                   <div>
-                    <label
-                      for="confirm-password"
-                      className="block mb-2 text-sm font-medium text-secondary "
-                    >
-                      Confirm password
+                    <label for="input">
+                      {" "}
+                      How did you hear about us?(Required)
                     </label>
-                    <input
-                      type="confirm-password"
-                      name="confirm-password"
-                      id="confirm-password"
-                      placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    "
-                      required=""
-                    />
+
+                    <select className=" select select-bordered  w-full ">
+                      <option selected>Instagram</option>
+                      <option>Facebook</option>
+                      <option>CottageCare Website</option>
+                      <option>Nextdoor</option>
+                      <option>Referral</option>
+                      <option>Indeed</option>
+                      <option>Other</option>
+                    </select>
                   </div>
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
+                  <div>
+                    <div>
+                      <label for="input"> Resume</label>
                       <input
-                        id="terms"
-                        aria-describedby="terms"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300   cursor-pointer"
-                        required=""
+                        type="file"
+                        className="file-input file-input-bordered file-input-secondary w-full "
                       />
                     </div>
-                    <div className="ml-3 text-sm">
-                      <label
-                        for="terms"
-                        className="font-light text-gray-500 dark:text-gray-300"
-                      >
-                        I accept the{" "}
-                        <a
-                          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                          href="#"
-                        >
-                          Terms and Conditions
-                        </a>
-                      </label>
-                    </div>
+                    <small>Max. file size: 50 MB.</small>
                   </div>
+
                   <button
                     type="submit"
                     className="w-full text-accent bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
                   >
-                    Create an account
+                    Submit
                   </button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Already have an account?{" "}
-                    <a
-                      href="#"
-                      className="font-medium text-primary-600 hover:underline "
-                    >
-                      Login here
-                    </a>
-                  </p>
                 </form>
               </div>
             </div>
