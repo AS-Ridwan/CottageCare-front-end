4e132a8fe50a9f4d79bb06e4c231a01043055cf2
hipopridwan <ridwanhipop@gmail.com>
2024-02-15 21:48:45 +0600

    employment page img and form added

--- src/pages/EmploymentOpportunities/EmploymentOpportunities.jsx
+++ src/pages/EmploymentOpportunities/EmploymentOpportunities.jsx
@@ -1,13 +1,138 @@
 import React from "react";
 
+import employment from "../../assets/employment.png";
+
 const EmploymentOpportunities = () => {
   return (
     <>
-      <div>EmploymentOpportunities page</div>
-      <div>EmploymentOpportunities page</div>
-      <div>EmploymentOpportunities page</div>
-      <div>EmploymentOpportunities page</div>
-      <div>EmploymentOpportunities page</div>
+      <section className="w-screen 2xl:px-64 mt-6 ">
+        <div>EmploymentOpportunities page</div>
+        <div className=" grid grid-cols-1 sm:grid-cols-2 items-center mt-24">
+          {/* left side img */}
+          {/* left side img */}
+          <div className="h-auto max-w-lg w-2/3  lg:w-screen ">
+            <img src={employment} alt="" />
+          </div>
+
+          {/* right side form */}
+          {/* right side form */}
+          <div className="flex flex-col items-end justify-center">
+            <div className="w-full bg-accent rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-xl font-medium leading-tight tracking-tight text-secondary  md:text-2xl ">
+                  Create your account to get full access!
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label
+                      for="text"
+                      className="block mb-2 text-sm font-medium text-secondary dark:text-accent"
+                    >
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      placeholder="name"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="email"
+                      className="block mb-2 text-sm font-medium text-secondary dark:text-accent"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="password"
+                      className="block mb-2 text-sm font-medium text-secondary "
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="confirm-password"
+                      className="block mb-2 text-sm font-medium text-secondary "
+                    >
+                      Confirm password
+                    </label>
+                    <input
+                      type="confirm-password"
+                      name="confirm-password"
+                      id="confirm-password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5    "
+                      required=""
+                    />
+                  </div>
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="terms"
+                        aria-describedby="terms"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300   cursor-pointer"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        for="terms"
+                        className="font-light text-gray-500 dark:text-gray-300"
+                      >
+                        I accept the{" "}
+                        <a
+                          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                          href="#"
+                        >
+                          Terms and Conditions
+                        </a>
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full text-accent bg-secondary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
+                  >
+                    Create an account
+                  </button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Already have an account?{" "}
+                    <a
+                      href="#"
+                      className="font-medium text-primary-600 hover:underline "
+                    >
+                      Login here
+                    </a>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -3,11 +3,11 @@ import React, { useEffect, useState } from "react";
 import instance from "../../api";
 
 const Service = () => {
-  const [services, setServices] = useState([]);
-  console.log(services);
-  useEffect(() => {
-    instance.get("/service").then((res) => setServices(res));
-  }, []);
+  // const [services, setServices] = useState([]);
+  // console.log(services);
+  // useEffect(() => {
+  //   instance.get("/service").then((res) => setServices(res));
+  // }, []);
 
   return <div>Service</div>;
 };
