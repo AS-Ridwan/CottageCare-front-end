29948af9862d92802853203b0f3c065bd7976d34
hipopridwan <ridwanhipop@gmail.com>
2024-06-29 19:05:26 +0600

    work updated

--- src/pages/Home/Detail.jsx
+++ src/pages/Home/Detail.jsx
@@ -3,61 +3,71 @@ import React from "react";
 const Detail = () => {
   return (
     <>
-      <div className="my-20 py-20 bg-accent">
-        <div className="grid grid-cols-3   2xl:px-64">
-          <div className="card w-96 bg-gray-200">
-            <div className="card-body">
-              <div className="w-14 h-14 bg-primary p-3 rounded-full mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#ffff"
-                  viewBox="0 0 24 24"
-                  stroke-width="0"
-                  stroke="currentColor"
-                  className="w-8 h-8"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-                  />
-                </svg>
-              </div>
-
-              <h2 className="card-title font-semibold text-2xl text-black">
-                Instant Quotes
-              </h2>
-              <p className="leading-8 text-black">
+      <div className=" flex justify-center items-center h-[500px]  2xl:px-64 my-24">
+        <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+          <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+          <div className="relative z-10 mx-auto max-w-md">
+            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#ffff"
+                viewBox="0 0 24 24"
+                stroke-width="0"
+                stroke="currentColor"
+                className="h-10 w-10 text-white transition-all "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
+                />
+              </svg>
+            </span>
+            <div className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+              <h2 className=" font-semibold text-2xl">Instant Quotes</h2>
+              <p>
                 Other cleaning companies perform a walkthrough before giving you
                 an estimate. CottageCare was the first to innovate
                 over-the-phone pricing. We provide estimates solely based on the
                 size of your home and the frequency of your cleanings.
               </p>
             </div>
-          </div>
-          <div className="card w-96 bg-gray-200">
-            <div className="card-body">
-              <div className="w-14 h-14 bg-primary p-3 rounded-full mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#0984e3"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#ffff"
-                  className="w-8 h-8 "
+            <div className="pt-5 text-base font-semibold leading-7">
+              <p>
+                <a
+                  href="#"
+                  className="text-sky-500 transition-all duration-300 group-hover:text-white"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              </div>
+                  Read the docs &rarr;
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+          <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+          <div className="relative z-10 mx-auto max-w-md">
+            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
+              {/* --- */}
 
-              <h2 className="card-title font-semibold text-2xl text-black">
-                No Rescheduling Fees
-              </h2>
-              <p className="leading-8 text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#0984e3"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#ffff"
+                className="h-10 w-10 text-white transition-all "
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </span>
+            <div className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+              <h2 className=" font-semibold text-2xl">No Rescheduling Fees</h2>
+              <p>
                 Your life is busy. Your schedule changes. The next thing you
                 know, people are visiting when you least expect it. We get it!
                 We understand that our customers’ lives are busy, and your
@@ -65,60 +75,57 @@ const Detail = () => {
                 reschedule? Not a problem.
               </p>
             </div>
-          </div>
-          <div className="card w-96 bg-gray-200">
-            <div className="card-body">
-              <div className="w-14 h-14 bg-primary p-3 rounded-full mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#ffff"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#0984e3"
-                  className="w-8 h-8"
+            <div className="pt-5 text-base font-semibold leading-7">
+              <p>
+                <a
+                  href="#"
+                  className="text-sky-500 transition-all duration-300 group-hover:text-white"
                 >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-                  />
-                </svg>
-              </div>
-
-              <h2 className="card-title font-semibold text-2xl text-black">
-                No Contracts
-              </h2>
-              <p className="leading-8 text-black">
+                  Read the docs &rarr;
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
+          <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+          <div className="relative z-10 mx-auto max-w-md">
+            <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#ffff"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="#0984e3"
+                className="h-10 w-10 text-white transition-all"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                />
+              </svg>
+            </span>
+            <div className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
+              <h2 className=" font-semibold text-2xl">No Contracts</h2>
+              <p>
                 We realize our customers don’t want to get locked into lengthy
                 contracts. Sometimes when you’re stuck in a contract, the
                 quality can deteriorate over time. We choose to earn your
                 loyalty with each and every cleaning.
               </p>
             </div>
+            <div className="pt-5 text-base font-semibold leading-7">
+              <p>
+                <a
+                  href="#"
+                  className="text-sky-500 transition-all duration-300 group-hover:text-white"
+                >
+                  Read the docs &rarr;
+                </a>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="text-center mt-10 text-secondary">
-          <p>Flexible Scope of Work to Meet Your Desired Budget</p>
-          <p className="flex justify-center items-center mt-4 font-semibold">
-            Call{" "}
-            <span className="w-10 h-10 bg-primary flex justify-center items-center rounded-full mx-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#fff"
-                viewBox="0 0 24 24"
-                stroke-width="0"
-                stroke="currentColor"
-                class="w-5 h-5 "
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
-                />
-              </svg>
-            </span>{" "}
-            1-800-476-7830
-          </p>
         </div>
       </div>
     </>
